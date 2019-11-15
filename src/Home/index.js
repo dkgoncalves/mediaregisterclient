2d4b8922da44968784eb48cc540aa442416e0ca8
Derik <derikpessoal@gmail.com>
2019-11-15 17:38:28 -0300

    Creating Home component

---
+++ src/Home/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, Card, Button, Row, Col } from 'react-materialize'
+
+const Home = () => (
+  <Container>
+    <Card>
+      <Row style={{ textAlign: 'center' }}>
+        <h2> Associação dos Programadores </h2>
+        <h5> Registre-se como membro para receber informações de TI toda a semana! </h5>
+
+      </Row>
+      <Row style={{ textAlign: 'center' }}>
+        <Col s={12} m={12}>
+          <Col s={12} m={6}>
+            <a href="/register" >
+              <Button style={{ width: '100%' }} > Registrar </Button>
+            </a>
+          </Col>
+          <Col s={12} m={6}>
+            <a href="/members">
+              <Button style={{ width: '100%' }} > Ver Membros </Button>
+            </a>
+          </Col>
+        </Col>
+      </Row>
+    </Card>
+  </Container>
+)
+
+export default Home

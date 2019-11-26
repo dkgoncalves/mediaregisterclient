2bdae942a11f647b5b7772b420f5bcb0143daaa2
Derik <derik@oagro.com.br>
2019-11-26 13:58:34 -0300

    Fixing paths

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ import ListMembers from './ListMembers'
 const Routes = () => (
   <Router>
     <Route exact path="/mediaregisterclient/" component={Home} />
-    <Route path="/mediaregisterclient/register" component={Register} />
-    <Route path="/mediaregisterclient/members" component={ListMembers} />
+    <Route path="/mediaregisterclient/register/" component={Register} />
+    <Route path="/mediaregisterclient/members/" component={ListMembers} />
   </Router>
 )
 

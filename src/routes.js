ca5f18888947166beee707824c00af0a181cc1e1
Derik <derik@oagro.com.br>
2019-11-26 14:31:32 -0300

    Adjusting routes again

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home'
 import Register from './Register'
 import ListMembers from './ListMembers'
 
 const Routes = () => (
   <Router>
-    <Route exact path="/mediaregisterclient/" render={() => <Redirect to="/"  component={Home}/>} />
-    <Route path="/mediaregisterclient/register" component={Register} />
+    <Route exact path="/" component={Home} />
+    <Route path="/register" component={Register} />
     <Route path="/members" component={ListMembers} />
   </Router>
 )
